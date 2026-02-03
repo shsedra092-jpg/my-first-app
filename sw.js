805aa466e5764ede9804a1568bdd76b3b9681e6a
shsedra092-jpg <shsedra092@gmail.com>
2026-02-04 00:40:06 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,28 @@
-const CACHE_NAME = "my-first-app-v2";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js", "./app.js"];
+const CACHE_NAME = "my-first-app-v3";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./app.js"];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request).catch(() => caches.match("./index.html"))
+      );
+    })
   );
 });
